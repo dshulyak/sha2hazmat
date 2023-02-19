3cc44030a7b699afa8a04821ffc9b0dbc60257f3
Dmitry <yashulyak@gmail.com>
2023-02-19 11:05:46 +0200

    updated version

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::{fs::File, io::Read, time::SystemTime};
+use core::slice::{self};
+use std::{fs::File, hint::black_box, io::Read, time::SystemTime};
 
 use sha2::{
     compress256,
     digest::{generic_array::GenericArray, typenum::U64},
 };
 
-const H256_256: [u32; 8] = [
-    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+const H256_256_U8: [u8; 32] = [
+    103, 230, 9, 106, 133, 174, 103, 187, 114, 243, 110, 60, 58, 245, 79, 165, 127, 82, 14, 81,
+    140, 104, 5, 155, 171, 217, 131, 31, 25, 205, 224, 91,
 ];
 
 fn main() {
@@ -15,22 +17,25 @@ fn main() {
     f.read_exact(&mut buf).unwrap();
 
     let start = SystemTime::now();
+    let mut one: GenericArray<u8, U64> = GenericArray::default();
+    let mut two: GenericArray<u8, U64> = GenericArray::default();
     for chunk in buf.chunks_exact_mut(64) {
-        let blocks = [*GenericArray::<u8, U64>::from_slice(chunk)];
-        
+        one.copy_from_slice(chunk);
         for _ in 0..400 {
-            let mut state = H256_256.clone();
-            compress256(&mut state, &blocks);
+            two[..32].copy_from_slice(H256_256_U8.as_slice());
+            let state = unsafe {
+                let (_, state, _) = two.align_to_mut::<u32>();
+                state
+            };
+            compress256((&mut state[..8]).try_into().unwrap(), slice::from_ref(&one));
             for i in 0..8 {
                 state[i] = state[i].to_be();
             }
-            let aligned = unsafe {
-                let (_, aligned, _) = state.align_to::<u8>();
-                aligned
-            };
-            chunk[..32].copy_from_slice(aligned);
+            (one, two) = (two, one);
         }
     }
+    black_box(one);
+    black_box(two);
     let elapsed = start.elapsed().unwrap();
     let n = buf.len() / 64;
     println!(
